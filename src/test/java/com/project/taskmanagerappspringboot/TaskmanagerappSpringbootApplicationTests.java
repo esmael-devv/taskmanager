ec92998bf095ea9a47a5b0a9a5671bc950d05f35
Ismail Mohammed <ismail.bitte@gmail.com>
2025-09-02 13:28:46 -0500

    setup

---
+++ src/test/java/com/project/taskmanagerappspringboot/TaskmanagerappSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.taskmanagerappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerappSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
